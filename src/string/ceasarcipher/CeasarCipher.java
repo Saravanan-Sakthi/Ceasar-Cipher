9df8d2e347f13578fb9b813a6bc5b2164b1286e0
Saravanan-Sakthi <saravanatesla@gmail.com>
2021-08-02 14:26:46 +0530

    1

--- src/string/ceasarcipher/CeasarCipher.java
+++ src/string/ceasarcipher/CeasarCipher.java
@@ -1,12 +1,12 @@
 package string.ceasarcipher;
 public class CeasarCipher {
-    void encrypt(String a, int key){
-        char [] A= a.toCharArray();
-        int len=A.length;
-        for (int i=0;i<len;i++){
-            int as=(int)A[i];
-            if (as>=65 && as<=90) System.out.print((char)(((as+key-65)%26)+65));
-            if (as>=97 && as<=122)System.out.print((char)(((as+key-97)%26)+97));
+    void encrypt(String input, int key){
+        char [] message = input.toCharArray();
+        int len= message.length;
+        for (int iterator = 0; iterator <len; iterator++){
+            int ascii =(int) message[iterator];
+            if (ascii >=65 && ascii <=90) System.out.print((char)(((ascii +key-65)%26)+65));
+            if (ascii >=97 && ascii <=122)System.out.print((char)(((ascii +key-97)%26)+97));
         }
     }
 }
--- src/string/ceasarcipher/Test.java
+++ src/string/ceasarcipher/Test.java
@@ -2,9 +2,11 @@ package string.ceasarcipher;
 import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
-        Scanner s= new Scanner(System.in);
-        String a= s.next();
-        int key= s.nextInt();
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the message : ");
+        String a= scan.next();
+        System.out.print("Enter the key : ");
+        int key= scan.nextInt();
         new CeasarCipher().encrypt(a,key);
     }
 }
